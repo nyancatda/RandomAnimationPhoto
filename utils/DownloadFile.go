0687fa4b91a896d0928392dcf77c0315da45676c
NyanCatda <nyancatda@outlook.com>
2021-08-14 23:23:54 +0800

    增加30秒超时

--- utils/DownloadFile.go
+++ utils/DownloadFile.go
@@ -7,21 +7,26 @@ import (
     "io"
     "path"
     "bufio"
+    "time"
 )
 
 //下载文件
 func GetFile(imgUrl string,imgPath string) {
     fileName := path.Base(imgUrl)
-    res, err := http.Get(imgUrl)
+	//设置30秒超时
+    client := http.Client{
+    	Timeout: 30 * time.Second,
+    }
+    res, err := client.Get(imgUrl)
     if err != nil {
-      fmt.Println("A error occurred!")
-      return
+    	fmt.Println("A error occurred!")
+    	return
     }
     defer res.Body.Close()
     reader := bufio.NewReaderSize(res.Body, 32 * 1024)
     file, err := os.Create(imgPath + fileName)
     if err != nil {
-      panic(err)
+    	panic(err)
     }
     writer := bufio.NewWriter(file)
     io.Copy(writer, reader)
