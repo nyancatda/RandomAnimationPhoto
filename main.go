e4b2f3c0fc1167709a7c6828a0c6dae687cb35d7
NyanCatda <nyancatda@outlook.com>
2021-08-15 20:17:58 +0800

    增加多线程

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
     "os"
     "fmt"
+    "sync"
 )
 
 import "./utils"
@@ -11,23 +12,31 @@ import "./utils"
 //判断目录是否存在,不存在则创建
 func mkdirFolder(path string){
     _, err := os.Stat(path)
-    if err == nil {}
     if os.IsNotExist(err) {
-        os.Mkdir(path, 777)
+        os.Mkdir(path, 0777)
     }
 }
 
 func main() {
+    var wg sync.WaitGroup
     var num int
-
-    mkdirFolder("./img")
 
     fmt.Println("请问需要下载几张图片:")
     fmt.Scanf("%v", &num)
 
+    mkdirFolder("./img")
+
     for i:=1;i<=num;i++ {
         sexUrl := utils.GetSexUrl()
-        fmt.Println("已获取到图片链接:"+sexUrl+" ,正在下载")
-        utils.GetFile(sexUrl,"./img/")
+        fmt.Println("已获取到图片链接:"+sexUrl+" ,开始下载")
+        wg.Add(1)
+        //创建一个新线程下载图片
+        go func(i int) {
+            utils.GetFile(sexUrl,"./img/")
+            defer wg.Done()
+        }(i)
     }
+
+    fmt.Println("请等待所有下载线程执行结束")
+    wg.Wait()
 }
